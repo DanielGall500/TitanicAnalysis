e25db0d3229373a854eff660f0d12ca2606b6106
DannyGotTea <danielgallagher500@gmail.com>
2016-02-18 22:18:18 +0000

    Survivors and Non-Survivors Analysis

--- TitanicAnalysis.py
+++ TitanicAnalysis.py
@@ -1,4 +1,5 @@
 import unicodecsv
+import numpy as np
 
 def read_csv(filename):
     with open(filename, 'rb') as f:
@@ -9,3 +10,17 @@ titanic_data = read_csv('C:/Users/dano/Desktop/TitanicData.csv')
 
 #Questions to ask:
 #What factors made people more likely to survive?
+
+titanic_survivors = np.array([])
+titanic_non_survivors = np.array([])
+
+for person in titanic_data:
+    if (person['Survived'] == '1'):
+        titanic_survivors = np.append(titanic_survivors, person)
+    else:
+        titanic_non_survivors = np.append(titanic_non_survivors, person)
+        
+print "Survivors: " + len(titanic_survivors)
+
+print "Non-Survivors: " + len(titanic_non_survivors)
+
