7e1af1d4437a9d22a174c5a68d5eacf54c5782d2
DannyGotTea <danielgallagher500@gmail.com>
2016-02-21 12:53:21 -0800

    Average Function Implemented
    a function calculating the average of data in the titanic csv file has
    been created

--- TitanicAnalysis.py
+++ TitanicAnalysis.py
@@ -6,12 +6,12 @@ def read_csv(filename):
         reader = unicodecsv.DictReader(f)
         return list(reader)
 
-titanic_data = read_csv('C:/Users/dano/Desktop/TitanicData.csv')
+titanic_data = read_csv('C:/Users/dano/Desktop/TitanicAnalysis-master/TitanicData.csv')
 
-#Questions to ask:
-#What factors made people more likely to survive?
+#Data Columns: PassengerId, Survived, Pclass, Name, Sex, Age, SibSp, Parch, Ticket, Fare, Cabin, Embarked
 
 titanic_survivors = np.array([])
+
 titanic_non_survivors = np.array([])
 
 for person in titanic_data:
@@ -20,7 +20,28 @@ for person in titanic_data:
     else:
         titanic_non_survivors = np.append(titanic_non_survivors, person)
         
-print "Survivors: " + len(titanic_survivors)
+print "Survivors: " + str(len(titanic_survivors))
 
-print "Non-Survivors: " + len(titanic_non_survivors)
+print "Non-Survivors: " + str(len(titanic_non_survivors))
+
+def findAvg(listData, column):
+	output_list = []
+	for data in listData:
+
+		column = data[column]
+
+		if(column == '' or column == ''):
+			continue
+		else:
+			output_list.append(float(column))
+		return sum(output_list) / len(output_list)
+
+
+print "Average age of a passenger: " + str((findAvg(titanic_data, 'Age'))) + "\n"
+print "Average age of a survivor: " + str((findAvg(titanic_survivors, 'Age'))) + "\n"
+print "Average age of a non-survivor: " + str((findAvg(titanic_non_survivors, 'Age'))) + "\n"
+
+
+
+
 
